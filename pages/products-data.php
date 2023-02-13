a815e728a1f9a2f704dc78409c12e4f6f109266f
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2023-02-13 19:01:24 +0200

    fixed redirect

--- pages/products-data.php
+++ pages/products-data.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
 <?php
 session_start();
 
 if (isset($_GET['name']) && isset($_GET['price'])) {
     $_SESSION['names'][] = $_GET['name'];
     $_SESSION['prices'][] = $_GET['price'];
-    echo 'The product has been successfully added to cart . <br />';
 }
-?>
 
-<a href="products.php">Back to shopping</a>
-<a href="cart.php">Go to cart</a>
-</body>
-</html>
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'products.php';
+header("Location: http://$host$uri/$extra");
+exit;
+
 
